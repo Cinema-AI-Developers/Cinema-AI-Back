766b6733f50a8b5144987a2abcdaf24ddc8dc0d7
Svoboda21 <zotova_2003@bk.ru>
2023-03-13 02:54:55 +0300

    Some functions add

--- data/users.py
+++ data/users.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, VARCHAR, Text, DateTime
 from flask_login import UserMixin
@@ -7,23 +7,35 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from db_session import SqlAlchemyBase
 
+
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'Users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nickname = Column(String, nullable=False)
-    description = Column(Text)
+    type = Column(String, nullable=False)
     email = Column(VARCHAR, unique=True, nullable=False)
     phone = Column(VARCHAR, unique=True)
-    hashed_password = Column(String, nullable=False)
-    created_date = Column(DateTime, default=datetime.datetime.now)
-    friends_ID = Column(String, nullable=True)
-    films_ID = Column(String, nullable=True)
     avatar = Column(String)
     gender = Column(String)
-    type = Column(String, nullable=False)
+    nickname = Column(String, nullable=False)
+    films_ID = Column(String, nullable=True)
+    friends_ID = Column(String, nullable=True)
+    description = Column(Text)
+    created_date = Column(DateTime, default=datetime.now)
+    hashed_password = Column(String, nullable=False)
 
     def __repr__(self):
+        """
+        return type if do print()
+        :return: string with description
+        """
+        return f"<User> {self.id} {self.nickname} {self.email}"
+
+    def __str__(self):
+        """
+        return type if str()
+        :return: string with description
+        """
         return f"<User> {self.id} {self.nickname} {self.email}"
 
     def set_password(self, password):
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_login import LoginManager, login_user, login_required, \
     logout_user, current_user
 from flask_restful import Api
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import PasswordField, BooleanField, SubmitField, StringField, \
+    TextAreaField, SelectField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 
 from data.__all_models import *
 from data import db_session
@@ -14,10 +20,64 @@ db_session.global_init("db/CinemaAI.sqlite")
 session = db_session.create_session()
 
 
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[Email(), DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль',
+                                   validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    gender = SelectField('gender', validators=[DataRequired()])
+    description = StringField("Немного о себе")
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return session.query(User).get(user_id)
 
+
 @app.route("/")
 def index():
     return render_template("index.html", title='Лента')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = session.query(User).filter(
+            User.email == form.email.data).first()
+        print(user)
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    print(datetime.datetime.now(), current_user.name, "id: ", current_user.id,
+          "вошел")
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/api/add_comm/<film_id>', methods=['POST'])
+@login_required
+def leave_comment(film_id):
+    text = request.form.get("text")
+    comm = Comment()
+    comm.user_ID = current_user.id
+    comm.film_ID = film_id
+    comm.text = text
+
+
+@app.route('/api/edit_comm/<comm_id>', methods=['POST'])
+def edit_comment(comm_id):
+    text = request.form.get("text")
+    comm = session.query(Comment).filter(Comment.id == comm_id)
+    comm.text = text
